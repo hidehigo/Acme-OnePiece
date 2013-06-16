52666ba0a702cc992ca45fe032191e93f226d708
hidehigo <hidehigo@cpan.org>
2013-06-16 11:00:26 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::OnePiece - It's new $module
+
+# SYNOPSIS
+
+    use Acme::OnePiece;
+
+# DESCRIPTION
+
+Acme::OnePiece is ...
+
+# LICENSE
+
+Copyright (C) hidehigo.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+hidehigo <hidehigo@cpan.org>
---
+++ lib/Acme/OnePiece.pm
@@ -0,0 +1,39 @@
+package Acme::OnePiece;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::OnePiece - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::OnePiece;
+
+=head1 DESCRIPTION
+
+Acme::OnePiece is ...
+
+=head1 LICENSE
+
+Copyright (C) hidehigo.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+hidehigo E<lt>hidehigo@cpan.orgE<gt>
+
+=cut
+
